a498b55a9e231fd2fdf8c1e8a866d83acc6e8a1e
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-12-21 11:56:45 +0700

    UI implementation

---
+++ app/src/main/java/com/keio/automapay/ui/resultpayment/PaymentResultActivity.kt
@@ -0,0 +1,12 @@
+package com.keio.automapay.ui.resultpayment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.keio.automapay.R
+
+class PaymentResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_result)
+    }
+}
--- app/src/main/java/com/keio/automapay/util/DummyData.kt
+++ app/src/main/java/com/keio/automapay/util/DummyData.kt
@@ -1,4 +1,39 @@
 package com.keio.automapay.util
 
+import com.keio.automapay.core.data.domain.model.ExpenditureModel
+import com.keio.automapay.core.data.domain.model.SplitBillModel
+import com.keio.automapay.core.data.domain.model.UtilityBillsModel
+
 object DummyData {
+
+    fun getDummyUtilityBills(): List<UtilityBillsModel>{
+        val listUtility = ArrayList<UtilityBillsModel>()
+        val data1 = UtilityBillsModel()
+        listUtility.add(data1)
+        val data2 = UtilityBillsModel()
+        listUtility.add(data2)
+        val data3 = UtilityBillsModel()
+        listUtility.add(data3)
+        return listUtility
+    }
+
+    fun getDummySplitBills(codeBills: String): SplitBillModel?{
+        val code = ""
+        return if (codeBills == code) {
+            SplitBillModel()
+        } else {
+            null
+        }
+    }
+
+    fun initExpenditure(): List<ExpenditureModel>{
+        val listExpenditure = ArrayList<ExpenditureModel>()
+        val data1 = ExpenditureModel()
+        listExpenditure.add(data1)
+        val data2 = ExpenditureModel()
+        listExpenditure.add(data2)
+        val data3 = ExpenditureModel()
+        listExpenditure.add(data3)
+        return listExpenditure
+    }
 }
